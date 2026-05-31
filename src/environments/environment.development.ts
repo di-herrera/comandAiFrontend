fcc34d088f53bc712ec60081ee912dc1b08a226d
di-herrera <herrera.ia@outlook.com>
2026-05-31 00:24:37 -0300

    feat: update API base URLs for development and production environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://acts-object-geek-issued.trycloudflare.com'
+  apiBaseUrl: 'http://localhost:5080'//'https://acts-object-geek-issued.trycloudflare.com'
 };
